5f948053e5d753cabcb16ca3df65567396068e23
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-09-13 12:18:01 -0400

    having a syntax error somewhere in my js but instead of using a onlick im using objects instead

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-$('#value').onClick() {
-    if
-}
+$(() => {
+  const sportsQuestions = [
+    {
+      questions: "Who won the world series in 2016",
+      answer: "Chicago Cubs",
+      choices: "New York Yankees, Chicago Cubs, Cleveland Indians, ",
+      points: 200,
+    },
+    {
+      questions: "What team lost a 28-3 lead in the Super Bowl",
+      answer: "Atlanta Falcons",
+      choices: "Atlanta Falcons, New England Patriots, Green Bay Packers, ",
+      points: 300,
+    },
+    
+    {
+      questions: "What basketball player had 42 triple doubles in the 2016-2017 year",
+      answer: "Russell Westbrook",
+      choices: "Stephen Curry, Lebron James, Russell Westbrook, ",
+      points: 300,
+    },
+    
+
+  ]
+  
+
+
+$('.basic').on('click', function ($event) {
+  const y = prompt(sportsQuestions[0].questions, `Type ${sportsQuestions[0].choices}`);
+if(y === sportsQuestions[0].answer) {
+  window.alert('Correct')
+} else {
+  window.alert('incorrect')
+
+    }
+  }
+};
